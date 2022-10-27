277c10e0ed0e1de84b1249b689758b7197fb7f52
Sam Der <samder713@gmail.com>
2022-10-27 15:06:42 -0700

    Removed dependency on flask_restful and flask_pymongo and updated /user routes and corresponding request methods

--- db.py
+++ db.py
@@ -1,3 +1,8 @@
-from flask_pymongo import PyMongo
+import os
 
-mongo = PyMongo()
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv('.env.sample')
+
+client = MongoClient(os.getenv("MONGO_URI_MASTER"))
