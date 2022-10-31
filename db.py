3421a41adfa1d2fdb98f4148a893b14330bcb138
Sam Der <samder713@gmail.com>
2022-10-31 15:09:50 -0700

    Added documentation

--- db.py
+++ db.py
@@ -1,8 +1,18 @@
+"""
+This file initializes a Python MongoDB client that can be shared across files.
+Since the only other code file is `app.py`, the code below could be moved to
+`app.py`, but with larger codebases where multiple files interact with the
+MongoDB client, this file will come in handy.
+"""
+
 import os
 
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
+# This is only a sample, so the `.env.sample` is used. For your project, you should
+# use `.env`. BE SURE TO INCLUDE THIS FILE IN YOUR `.gitignore` FILE OR ELSE YOUR
+# MONGODB CREDENTIALS WILL BE PUBLISHED TO GITHUB!
 load_dotenv('.env.sample')
 
 client = MongoClient(os.getenv("MONGO_URI_MASTER"))
